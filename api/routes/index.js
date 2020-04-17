69457209eef2650509891fdc1e90db0190b83524
Eugene Kartavchenko <ewg@gismeteo.ru>
2020-04-17 22:54:18 +0300

    удаляем из репы то, что не отновится к фронтенд-клиенту телеграма

--- api/routes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (router) => {
-    require('./goals')(router);
-    require('./contracts')(router);
-    require('./commits')(router);
-    require('./users')(router);
-    require('./auth')(router);
-}
